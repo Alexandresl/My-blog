1e41cb66a0e63c6845d5eb93a7fe44a0317602fa
Alexandre Lima <limaalexguitar@gmail.com>
2019-10-19 18:40:18 -0300

    Limpando os arquivos iniciais

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Alexandre Lima`,
+    description: `Aqui você encontra informações sobre desenvolvimento web`,
+    author: `Alexandre Lima`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
