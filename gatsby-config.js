dfda68603969d86c41e3ea8c16b4ef89184e9971
Alexandre Lima <al3xandr3sl@gmail.com>
2021-03-27 21:54:33 -0300

    7. Organizando os arquivos

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Alexandre Lima - My blog`,
+    description: `A blog about fontend development and other cool stuff`,
+    author: `@alexandresl`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
