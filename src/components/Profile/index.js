5740256d0df5e98e7b94e71fbd8ba2c753215b6b
Alexandre Lima <al3xandr3sl@gmail.com>
2021-04-03 00:12:43 -0300

    10. Criando um componente e compartilhando entre páginas

---
+++ src/components/Profile/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Profile = () => (
+  <div className="Profile-wrapper">
+    <h1>Alexandre Lima</h1>
+    <h2>Frontend Developer</h2>
+    <p>Membro do Core Team da empresa Foo, Escrevo sobre o mundo do frontend.</p>
+  </div>
+);
+
+export default Profile
