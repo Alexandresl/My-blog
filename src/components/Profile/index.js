aedba21a464f7afb6e0d09814a13d902f6e41c8e
Alexandre Lima <al3xandr3sl@gmail.com>
2021-05-03 02:12:53 -0300

    10. Criando um componente e compartilhando entre páginas

---
+++ src/components/Profile/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Profile = () => (
+  <div className="Profile-wrapper">
+    <h1>Alexandre Lima</h1>
+  </div>
+)
+
+export default Profile;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import * as React from "react"
-import { Link } from 'gatsby'
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
@@ -8,14 +7,6 @@ const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
     <h1>Home</h1>
-    <ul>
-      <li>
-        <Link to="/" activeStyle={{ color: 'red'}}>Home</Link>
-      </li>
-      <li>
-        <Link to="/about" activeStyle={{ color: 'red'}}>About</Link>
-      </li>
-    </ul>
   </Layout>
 )
 
